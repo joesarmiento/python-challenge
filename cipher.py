de3da5ba19800f77ba866c65b64ae75bf0b19efb
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2018-01-26 23:08:13 +0000

    Only encode alpha chars

--- cipher.py
+++ cipher.py
@@ -1,11 +1,14 @@
 class Cipher(object):
-    def __init__(self, cipher):
-        self.cipher = cipher 
+    def __init__(self, cipherValue):
+        self.cipherValue = cipherValue
+
+    def cipher(self, c):
+        asciiValue = ord(c)
+        if asciiValue >= 65 and asciiValue <= 90:
+            return chr(asciiValue + self.cipherValue)
+        else:
+            return c
 
     def encode(self, s):
         characters = list(s.upper())
-        result = ''
-        for c in characters:
-            result = result + chr(ord(c) + self.cipher)
-
-        return result
+        return ''.join(map(self.cipher, characters))
